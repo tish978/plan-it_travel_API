d4da938a2ffff9e41f1584fac915ba9b0e482f6f
tish978 <satish.bisa@gmail.com>
2023-06-26 10:57:59 -0700

    Login and Signup API functionality.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pymongo
 from database import db
 
 app = FastAPI()
+database = db['plan-it_travel']
 
 
 @app.get("/")
@@ -21,7 +22,7 @@ async def say_hello(name: str):
 
 @app.get("/test")
 async def list_users():
-    database = db['plan-it_travel']
+
     col = database['users']
 
     user_list = []
@@ -33,3 +34,30 @@ async def list_users():
     return "Success"
 
 
+@app.post("/sign-up")
+def sign_up(username: str, password: str):
+    collection = database['users']
+
+    existing_user = collection.find_one({"username": username})
+    if existing_user:
+        return {"message": "Username already exists"}
+
+    # Create a document to insert
+    user_doc = {
+        "username": username,
+        "password": password
+    }
+
+    collection.insert_one(user_doc)
+
+    return {"message": "User created successfully."}
+
+@app.post("/login")
+def login(username: str, password: str):
+    collection = database['users']
+
+    existing_user = collection.find_one({"username": username, "password": password})
+    if existing_user:
+        return {"message": "Successful login!"}
+    else:
+        return{"message": "ERROR: Username or Password is not correct."}
