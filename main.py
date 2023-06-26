de95f55dd4943ac2b8c6dee73d109c384e3f0efc
tish978 <satish.bisa@gmail.com>
2023-06-26 11:23:33 -0700

    HTML page created for sign_up functionality.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import connection
 from bson import ObjectId
 from schematics.models import Model
 import pymongo
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from database import db
 
 app = FastAPI()
 database = db['plan-it_travel']
-
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
@@ -20,37 +22,27 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-@app.get("/test")
-async def list_users():
-
-    col = database['users']
-
-    user_list = []
-    for x in col.find({}, {}):
-        user_list.append(x)
-
-    print(user_list)
-
-    return "Success"
+@app.get("/sign-up", response_class=HTMLResponse)
+async def sign_up(request: Request):
+    return templates.TemplateResponse("sign-up.html", {"request": request})
 
 
 @app.post("/sign-up")
-def sign_up(username: str, password: str):
-    collection = database['users']
+async def handle_sign_up(request: Request):
+    form_data = await request.form()
+    username = form_data.get("username")
+    password = form_data.get("password")
 
+    # Check if username already exists
+    collection = database['users']
     existing_user = collection.find_one({"username": username})
     if existing_user:
         return {"message": "Username already exists"}
 
-    # Create a document to insert
-    user_doc = {
-        "username": username,
-        "password": password
-    }
+    # Insert username and password into the collection
+    collection.insert_one({"username": username, "password": password})
+    return {"message": "User registered successfully"}
 
-    collection.insert_one(user_doc)
-
-    return {"message": "User created successfully."}
 
 @app.post("/login")
 def login(username: str, password: str):
