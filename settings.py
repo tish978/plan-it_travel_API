4c171c467a63a44d570234bb4d047af11c66dc71
tish978 <satish.bisa@gmail.com>
2023-11-04 11:25:46 -0700

    Plan Trip fixes

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 # MongoDB attributes
 #mongodb_uri = 'mongodb+srv://satishbisa:Hawkins!123@plan-it-travel-cluster.hzlywo7.mongodb.net/'
-mongodb_uri = 'mongodb+srv://satishbisa:hitootie!123@cluster0.b7fpk09.mongodb.net/?retryWrites=true&w=majority'
+#mongodb_uri = 'mongodb+srv://satishbisa:hitootie!123@cluster0.b7fpk09.mongodb.net/?retryWrites=true&w=majority'
+mongodb_uri = 'mongodb+srv://satishbisa:hitootie!123@cluster0.b7fpk09.mongodb.net/?retryWrites=true&w=majority&ssl=true'
 port = 8000
 
